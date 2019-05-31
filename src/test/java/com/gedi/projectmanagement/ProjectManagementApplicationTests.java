8723b00d591377f9419a3e68d66d92936c115515
zs344356022 <344356022@qq.com>
2019-05-31 15:40:11 +0800

    更新

--- src/test/java/com/gedi/projectmanagement/ProjectManagementApplicationTests.java
+++ src/test/java/com/gedi/projectmanagement/ProjectManagementApplicationTests.java
@@ -7,10 +7,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProjectManagementApplicationTests {
-
 
     @Test
     public void contextLoads() {
