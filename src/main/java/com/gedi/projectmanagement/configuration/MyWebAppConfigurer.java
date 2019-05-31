b1628bb36890bbbdc79341b457aba4938597821d
zs344356022 <344356022@qq.com>
2019-05-31 17:26:36 +0800

    更新

---
+++ src/main/java/com/gedi/projectmanagement/configuration/MyWebAppConfigurer.java
@@ -0,0 +1,7 @@
+package com.gedi.projectmanagement.configuration;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class MyWebAppConfigurer extends WebMvcConfigurerAdapter {
+
+}
