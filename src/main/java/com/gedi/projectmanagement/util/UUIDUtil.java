5c4a67c43d879ef38a08d2aa0b8dec147c4fe50c
zs344356022 <344356022@qq.com>
2019-06-03 10:06:06 +0800

    UUID

--- src/main/java/com/gedi/projectmanagement/util/UUIDUtil.java
+++ src/main/java/com/gedi/projectmanagement/util/UUIDUtil.java
@@ -2,6 +2,13 @@ package com.gedi.projectmanagement.util;
 
 import java.util.UUID;
 
+/**
+ * @ClassName: UUID
+ * @Description: 为了让数据库里的主键生成UUID
+ * @Author zhangshuai
+ * @Date 2019-06-03 10:04:54
+ * @Version 1.0
+ **/
 public class UUIDUtil {
 
     /**
