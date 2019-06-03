54a35ec803d2dcd0cd93dec40ccd38b3fc3afb8a
wangyanbin <w_yanbin@qq.com>
2019-06-03 17:09:13 +0800

    公用vo类

--- src/main/java/com/gedi/projectmanagement/vo/CodeAndMsgUtil.java
+++ src/main/java/com/gedi/projectmanagement/vo/CodeAndMsgUtil.java
@@ -1,5 +1,9 @@
 package com.gedi.projectmanagement.vo;
 
+/**
+ * @author wangyanbin
+ * @description 返回类型工具类 1000表示成功  2000表示失败
+ */
 public class CodeAndMsgUtil {
     public static CodeAndMsg setOK(String msg, Object data){
         return new CodeAndMsg(1000,msg,data);
