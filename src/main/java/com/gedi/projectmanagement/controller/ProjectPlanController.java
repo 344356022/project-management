36cf05e746910427cc6b8b71c66d3a716d49c4f6
zs344356022 <344356022@qq.com>
2019-06-04 11:17:38 +0800

    项目总计划清单的接口

--- src/main/java/com/gedi/projectmanagement/controller/ProjectPlanController.java
+++ src/main/java/com/gedi/projectmanagement/controller/ProjectPlanController.java
@@ -3,8 +3,10 @@ package com.gedi.projectmanagement.controller;
 import com.gedi.projectmanagement.service.ProjectPlanService;
 import com.gedi.projectmanagement.vo.CodeAndMsg;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
@@ -12,17 +14,23 @@ import javax.annotation.Resource;
  * @author
  * @date
  */
-@Controller
+@RestController
 public class ProjectPlanController {
 
     @Resource
     private ProjectPlanService projectPlanService;
 
     //列表展示所有
-    @RequestMapping("/listAll")
-    @ResponseBody
-    public CodeAndMsg selectById(){
+    @PostMapping("/listAll")
+    public CodeAndMsg selectById() {
         System.out.println(projectPlanService.selectById() + "列表展示数据：");
         return projectPlanService.selectById();
     }
+
+    // 展示所有清单列表
+    @PostMapping(value = "/projectPlanList")
+    public CodeAndMsg selectProjectPlanListByPidAndPprojectphase(String pId, Integer pProjectPhase) {
+        return projectPlanService.selectProjectPlanListByPidAndPprojectphase(pId, pProjectPhase);
+    }
+
 }
