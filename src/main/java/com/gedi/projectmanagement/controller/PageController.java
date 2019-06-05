122ac9c7feb97b4abd6583e9933829dc23c57f7e
wangyanbin <w_yanbin@qq.com>
2019-06-05 11:43:30 +0800

    page

--- src/main/java/com/gedi/projectmanagement/controller/PageController.java
+++ src/main/java/com/gedi/projectmanagement/controller/PageController.java
@@ -4,10 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-@RestController
+@Controller
 public class PageController {
-
 
 	@RequestMapping("{page}")
 	public String page(@PathVariable("page") String page) {
