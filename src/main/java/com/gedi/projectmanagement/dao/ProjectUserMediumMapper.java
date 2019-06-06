a6119786d2670711f4504dfb2769529d42d32cdf
zs344356022 <344356022@qq.com>
2019-06-06 09:26:36 +0800

    更新

--- src/main/java/com/gedi/projectmanagement/dao/ProjectUserMediumMapper.java
+++ src/main/java/com/gedi/projectmanagement/dao/ProjectUserMediumMapper.java
@@ -1,7 +1,12 @@
 package com.gedi.projectmanagement.dao;
 
 import com.gedi.projectmanagement.model.ProjectUserMedium;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
+@Mapper
 public interface ProjectUserMediumMapper {
     int deleteByPrimaryKey(String id);
 
@@ -11,7 +16,25 @@ public interface ProjectUserMediumMapper {
 
     ProjectUserMedium selectByPrimaryKey(String id);
 
+    /**
+     * 项目总体计划清单的动态修改中间表
+     *
+     * @param record
+     * @return
+     */
     int updateByPrimaryKeySelective(ProjectUserMedium record);
 
     int updateByPrimaryKey(ProjectUserMedium record);
+
+    /**
+     * 根据任务项ID集合删除 任务项-用户 关系
+     * @param ids 任务项ID集合
+     */
+    void deleteByAids(@Param("list") List<String> ids);
+
+    /**
+     * 批量插入 任务项-人物关系
+     * @param userMediumList  任务集合
+     */
+    void batchInsert(@Param("list") List<ProjectUserMedium>  userMediumList);
 }
