c63bd9a5a1ba6fed512c9925bc13a20510f97997
Your Name <1279544372@qq.com>
2019-06-09 23:26:19 +0800

    对用户批量增添的接口予以实现

--- src/main/java/com/gedi/projectmanagement/service/impl/UserServiceImpl.java
+++ src/main/java/com/gedi/projectmanagement/service/impl/UserServiceImpl.java
@@ -1,12 +1,15 @@
 package com.gedi.projectmanagement.service.impl;
 
 import com.gedi.projectmanagement.dao.UserMapper;
+import com.gedi.projectmanagement.model.User;
 import com.gedi.projectmanagement.service.UserService;
 import com.gedi.projectmanagement.vo.CodeAndMsg;
 import com.gedi.projectmanagement.vo.CodeAndMsgUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 @Service
@@ -24,5 +27,32 @@ public class UserServiceImpl implements UserService {
         }else{
             return CodeAndMsgUtil.setERROR("查询失败",null);
         }
+    }
+
+    @Override
+    public CodeAndMsg insertManyUserMessage(List<User> list) {
+
+        CodeAndMsg codeAndMsg=new CodeAndMsg();
+        if(list.size()!=0){
+            userMapper.insertManyUserMessage(list);
+            codeAndMsg.setMsg("添加成功");
+            codeAndMsg.setResult(true);
+            codeAndMsg.setCode(200);
+        }else {
+            codeAndMsg.setCode(400);
+            codeAndMsg.setResult(false);
+            codeAndMsg.setMsg("添加失败");
+        }
+        return codeAndMsg;
+    }
+
+    @Override
+    public List<User> selectAllUserId() {
+        return userMapper.selectAllUserId();
+    }
+
+    @Override
+    public void deleteAllUserMessage() {
+        userMapper.deleteAllUserMessage();
     }
 }
