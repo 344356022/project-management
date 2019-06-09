0552afc292f0e8d878a5caa37b2a9c3e8f4e7399
Your Name <1279544372@qq.com>
2019-06-09 23:23:07 +0800

    增添批量增添用户的接口

--- src/main/java/com/gedi/projectmanagement/service/UserService.java
+++ src/main/java/com/gedi/projectmanagement/service/UserService.java
@@ -1,9 +1,22 @@
 package com.gedi.projectmanagement.service;
 
+import com.gedi.projectmanagement.model.User;
 import com.gedi.projectmanagement.vo.CodeAndMsg;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface UserService {
 
     //根据部门以及标签进行查询部门普通员工
     CodeAndMsg selectUserBySign(String department);
+
+    //通过调用钉钉第三方接口，获取部门下的所有员工信息，批量增添到自己的数据库中进行储存；
+    CodeAndMsg insertManyUserMessage(@Param("list") List<User> list);
+
+    //查询用户的唯一表示主键id值；
+    List<User> selectAllUserId();
+
+    //删除表里的所有数据
+    void deleteAllUserMessage();
 }
