38f983d31a022c1b96065ccfa9d2f573f915d77f
Your Name <1279544372@qq.com>
2019-06-11 03:02:11 +0800

    更改对应的方法名称

--- src/main/java/com/gedi/projectmanagement/controller/WeekReportController.java
+++ src/main/java/com/gedi/projectmanagement/controller/WeekReportController.java
@@ -2,14 +2,18 @@ package com.gedi.projectmanagement.controller;
 
 import com.alibaba.fastjson.JSONArray;
 import com.gedi.projectmanagement.model.ActionItem;
+import com.gedi.projectmanagement.model.ProjectPlan;
 import com.gedi.projectmanagement.model.User;
 import com.gedi.projectmanagement.model.Weekreport;
 
+import com.gedi.projectmanagement.service.ProjectPlanService;
 import com.gedi.projectmanagement.service.TaskSubClassService;
 import com.gedi.projectmanagement.service.UserService;
 import com.gedi.projectmanagement.service.WeekReportService;
+import com.gedi.projectmanagement.util.DetialDayDate;
 import com.gedi.projectmanagement.util.UUIDUtil;
 import com.gedi.projectmanagement.vo.CodeAndMsg;
+import com.sun.tools.javac.jvm.Code;
 import org.apache.ibatis.annotations.Update;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -38,6 +42,9 @@ public class WeekReportController {
     private UserService userService;
 
     @Autowired
+    private ProjectPlanService projectPlanService;
+
+    @Autowired
     private TaskSubClassService taskSubClassService;
 
 
@@ -49,7 +56,7 @@ public class WeekReportController {
 
 
     //将周计划设定的具体内容进行保存；
-    @PostMapping(value = "addWeekReport", produces = "application/json;charset=UTF-8")
+    @PostMapping(value = "createMoreWeekReport", produces = "application/json;charset=UTF-8")
     public CodeAndMsg addWeekReport(String weekreports) {
         CodeAndMsg msg = new CodeAndMsg();
         if (StringUtils.isEmpty(weekreports)) {
@@ -89,7 +96,7 @@ public class WeekReportController {
 
 
     //根据日报员工手写的百分比进行与实际进行核算比例
-    @PatchMapping("selectProjectById")
+    @PatchMapping("updateWeekReportActualProportion")
     public CodeAndMsg selectProjectById(String wId,int wActualProportion){
           return weekReportService.updateActual(wId,wActualProportion);
     }
@@ -106,6 +113,30 @@ public class WeekReportController {
     @GetMapping("queryTaskSubIdAndName")
     public CodeAndMsg queryTaskSubIdAndName(){
        return taskSubClassService.queryTaskSubIdAndName();
+    }
+
+    //将连续十二天的具体所对应的日期进行响应给前台
+    @GetMapping("getTwelveDayDate")
+    public CodeAndMsg getTwelveDayDate(){
+        CodeAndMsg codeAndMsg=new CodeAndMsg();
+        if(DetialDayDate.getTweleveDayDate().size()!=0){
+            codeAndMsg.setCode(200);
+            codeAndMsg.setResult(true);
+            codeAndMsg.setData(DetialDayDate.getTweleveDayDate());
+        }else {
+            codeAndMsg.setCode(400);
+            codeAndMsg.setResult(false);
+            codeAndMsg.setMsg("获取失败");
+
+        }
+
+        return codeAndMsg;
+    }
+
+    //获取项目的项目名称以及ID值
+    @RequestMapping("selectProjectPlanIdAndName")
+    public CodeAndMsg selectProjectPlanIdAndName(){
+        return projectPlanService.selectAllProject();
     }
 
 
