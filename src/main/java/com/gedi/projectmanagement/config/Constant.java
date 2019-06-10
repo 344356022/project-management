9c15d9fa3a2892c3a01313b850b6760906f45a58
Your Name <1279544372@qq.com>
2019-06-10 09:22:36 +0800

    更新项目所对应的key值

--- src/main/java/com/gedi/projectmanagement/config/Constant.java
+++ src/main/java/com/gedi/projectmanagement/config/Constant.java
@@ -7,9 +7,9 @@ public class Constant {
     /**
      * 开发者后台->企业自建应用->选择您创建的E应用->查看->AppKey
      */
-    public static final String APP_KEY = "dingkxeodjgdmjowhgcr";
+    public static final String APP_KEY = "dingdkddaarp09a1dz9o";
     /**
      * 开发者后台->企业自建应用->选择您创建的E应用->查看->AppSecret
      */
-    public static final String APP_SECRET = "sTxnODhYQiUC3x4UJm9IUL1EpPwBc8Tbmj3zl3yibl8kIVM_9QYWC44zLL0VfWm3";
+    public static final String APP_SECRET = "0bixwcIuMFppsrVOLxtVYryuSdoDGjYRZeDhWth80ZgVHRuC-pR-q1ppAzsiK_uT";
 }
