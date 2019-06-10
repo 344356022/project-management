aba37138f710e030c00423b3618441972dc55254
Your Name <1279544372@qq.com>
2019-06-10 09:18:46 +0800

    此模块的mapper文件未改动

--- src/main/java/com/gedi/projectmanagement/dao/UserMapper.java
+++ src/main/java/com/gedi/projectmanagement/dao/UserMapper.java
@@ -2,6 +2,7 @@ package com.gedi.projectmanagement.dao;
 
 import com.gedi.projectmanagement.model.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -10,5 +11,14 @@ public interface UserMapper {
 
     //根据部门以及标签进行查询部门普通员工
     List<User> selectUserBySign(String department);
+
+    //通过调用钉钉第三方接口，获取部门下的所有员工信息，批量增添到自己的数据库中进行储存；
+    void insertManyUserMessage(@Param("list") List<User> list);
+
+    //查询用户的唯一表示主键id值；
+    List<User> selectAllUserId();
+
+    //删除表里的所有数据
+    void deleteAllUserMessage();
 
 }
