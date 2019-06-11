253302c0faded202115e36b2e1b95ff937b00ca6
Your Name <1279544372@qq.com>
2019-06-11 18:02:14 +0800

    手动维护用户表的实现

--- src/main/java/com/gedi/projectmanagement/service/UserService.java
+++ src/main/java/com/gedi/projectmanagement/service/UserService.java
@@ -9,7 +9,7 @@ import java.util.List;
 public interface UserService {
 
     //根据部门以及标签进行查询部门普通员工
-    CodeAndMsg selectUserBySign(String department);
+    CodeAndMsg selectUserBySign();
 
     //通过调用钉钉第三方接口，获取部门下的所有员工信息，批量增添到自己的数据库中进行储存；
     CodeAndMsg insertManyUserMessage(@Param("list") List<User> list);
