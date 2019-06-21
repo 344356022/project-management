90297161c77150e3426d77379d737e6a8266a860
Your Name <1279544372@qq.com>
2019-06-21 11:14:19 +0800

    增加定时器得生效注解

--- src/main/java/com/gedi/projectmanagement/ProjectManagementApplication.java
+++ src/main/java/com/gedi/projectmanagement/ProjectManagementApplication.java
@@ -2,11 +2,18 @@ package com.gedi.projectmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class ProjectManagementApplication {
+public class ProjectManagementApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ProjectManagementApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectManagementApplication.class, args);
