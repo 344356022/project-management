55c364cefc1d3d19191fc87660ceb09a511b1322
373458318 <zhouliuchuang@163.com>
2019-07-01 00:31:52 +0800

    日报功能开发

--- src/main/java/com/gedi/projectmanagement/dao/JournalMapper.java
+++ src/main/java/com/gedi/projectmanagement/dao/JournalMapper.java
@@ -13,77 +13,101 @@ import java.util.List;
  * @date 2019-06-03
  */
 @Mapper
-public interface JournalMapper {
-    /**
+public interface JournalMapper
+{
+	/**
      * 查询日志信息
      *
      * @param rbId 日志ID
      * @return 日志信息
      */
-    public Journal selectJournalById(String rbId);
+	public Journal selectJournalById(String rbId);
 
-    /**
+	/**
      * 查询日志列表
      *
      * @param journal 日志信息
      * @return 日志集合
      */
-    public List<Journal> selectJournalList(Journal journal);
+	public List<Journal> selectJournalList(Journal journal);
 
-    /**
+	/**
      * 新增日志
      *
      * @param journal 日志信息
      * @return 结果
      */
-    public int insertJournal(Journal journal);
+	public int insertJournal(Journal journal);
 
-    /**
+	/**
      * 修改日志
      *
      * @param journal 日志信息
      * @return 结果
      */
-    public int updateJournal(Journal journal);
+	public int updateJournal(Journal journal);
 
-    /**
+	/**
      * 删除日志
      *
      * @param rbId 日志ID
      * @return 结果
      */
-    public int deleteJournalById(String rbId);
+	public int deleteJournalById(String rbId);
 
-    /**
+	/**
      * 批量删除日志
      *
      * @param rbIds 需要删除的数据ID
      * @return 结果
      */
-    public int deleteJournalByIds(String[] rbIds);
+	public int deleteJournalByIds(String[] rbIds);
 
-    /**
-     * 返回日报列表
-     *
-     * @param map 需要用户信息
-     * @return
-     */
-    List<HashMap<String, Object>> selectJournalResultList(HashMap map);
+	/**
+	 * 返回日报列表
+	 * @param map 需要用户信息
+	 * @return
+	 */
+	List<HashMap<String, Object>> selectJournalResultList(HashMap map);
 
-    /**
-     * 返回日报接收人列表
-     *
-     * @return
-     */
-    List<HashMap<String, Object>> selectUserList();
+	/**
+	 * 返回日报接收人列表
+	 * @return
+	 */
+	List<HashMap<String, Object>> selectUserList();
 
-    /**
-     * 批量插入日报信息
-     *
-     * @param list
-     * @return
-     */
+	/**
+	 * 批量插入日报信息
+	 * @param list
+	 * @return
+	 */
     int insertJournalList(List list);
 
-    List<Journal> selectBypId(String pId);
+
+
+	/**
+	 * 查询日报记录列表
+	 *
+	 * @param map 日志信息
+	 * @return 日志集合
+	 */
+	public List<HashMap<String, Object>> selectJournalHistory(HashMap map);
+
+	/**
+	 * 查询我的团队的日报记录
+	 * @param map
+	 * @return
+	 */
+	public List<HashMap<String, Object>> querySelfTeamDaily(HashMap map);
+
+	/**
+	 * 查询所有的日报信息
+	 * @param map
+	 * @return
+	 */
+	public List<HashMap<String, Object>> queryAllDaily(HashMap map);
+
+
+	List<Journal> selectBypId(String pId);
+
 }
