55d943e8307454c26519d8dd0def703f2150bfca
373458318 <zhouliuchuang@163.com>
2019-07-02 16:08:05 +0800

    钉钉接口公共资源

---
+++ src/main/resources/templates/dingding.js
@@ -0,0 +1,43 @@
+var IP="106.13.23.64";  //测试：94.191.43.232   正式：
+var isDing=true;
+var ceShiCode='';
+
+//获取有关钉钉需要的值
+function  getDingDing() {
+    if(isDing){
+        var _this=this;
+        $.ajax({
+            url:"http://"+IP+":8082/journal/queryEmterpriseMesg",
+            type:"GET",
+            data:{},
+            dataType:"json",
+            success:function(res){
+                return   _this.getCode(res);
+            }
+        });
+
+    }else{
+        return ceShiCode;
+    }
+
+
+
+}
+
+//获取临时授权码,
+function  getCode(_config) {
+    var _this=this;
+    dd.ready(function() {
+        dd.runtime.permission.requestAuthCode({
+            corpId:_config.corpId, // 企业id
+            onSuccess: function (info) {
+                console.log(info)//临时授权码
+                return info.code;
+                //_this.getDailyList(info.code);
+            },
+            onFail : function(err) {
+                alert('fail: ' + JSON.stringify(err));
+            }
+        });
+    });
+}
